9c8305a52c01686a836676998931359f7f988d62
temur023 <temurmalikirgashev@gmail.com>
2026-03-08 01:21:57 +0500

    Added dark mode

--- Clean.Infrastructure/obj/Debug/net10.0/Clean.Infrastructure.AssemblyInfo.cs
+++ Clean.Infrastructure/obj/Debug/net10.0/Clean.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clean.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e2ac66962dd852169b6597c57f2f1ea63bc96f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bde070133327ca13c3e1cd235b1d2b7d195b5bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clean.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clean.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
