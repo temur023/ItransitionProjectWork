d15731bbc2f9c4042444502f7ec71237d79a6e38
temur023 <temurmaikirgashev@gmail.com>
2026-03-21 23:37:36 +0500

    added inventory aggregation statistics

---
+++ Clean.Infrastructure/Migrations/20260321183716_apiToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clean.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class apiToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiToken",
+                table: "Inventories",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiToken",
+                table: "Inventories");
+        }
+    }
+}
