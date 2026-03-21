b2bb8c2feabe073faf32efe2b094c838449de005
temur023 <temurmaikirgashev@gmail.com>
2026-03-21 17:24:13 +0500

    integrated salesforce

--- Clean.Application/Dtos/SalesforceDtos/SalesforceCreateDto.cs
+++ Clean.Application/Dtos/SalesforceDtos/SalesforceCreateDto.cs
@@ -2,8 +2,8 @@ namespace Clean.Application.Dtos.SalesforceDtos;
 
 public class SalesforceCreateDto
 {
-    public string AccessToken { get; set; }
-    public string InstanceUrl { get; set; }
+    public string? AccessToken { get; set; }
+    public string? InstanceUrl { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
